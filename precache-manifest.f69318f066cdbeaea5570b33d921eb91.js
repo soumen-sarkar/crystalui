a724b0abcd92f62537d3c350f9cca6b1777f9328
unknown <soumen.rim@gmail.com>
2020-06-08 19:57:58 +0530

    Updates

---
+++ precache-manifest.f69318f066cdbeaea5570b33d921eb91.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e00a9618da90d3977bc898d2697ea7f9",
+    "url": "/crystalui/index.html"
+  },
+  {
+    "revision": "5f6a3e8b2a77583cdd74",
+    "url": "/crystalui/static/css/main.3b5cb57a.chunk.css"
+  },
+  {
+    "revision": "0710da863377629b7bd8",
+    "url": "/crystalui/static/js/2.d7f6030b.chunk.js"
+  },
+  {
+    "revision": "8a666dd660e108ab4f1e0f22fb2e9cd2",
+    "url": "/crystalui/static/js/2.d7f6030b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5f6a3e8b2a77583cdd74",
+    "url": "/crystalui/static/js/main.eb7a569a.chunk.js"
+  },
+  {
+    "revision": "c14035cd2d02cf5bf7da",
+    "url": "/crystalui/static/js/runtime-main.4f8e20cb.js"
+  },
+  {
+    "revision": "fe9f4e04b009e13dceb57de2acf5b238",
+    "url": "/crystalui/static/media/bg.fe9f4e04.jpg"
+  },
+  {
+    "revision": "151c4696847a325b6f5a672ec0c426bc",
+    "url": "/crystalui/static/media/gothic.151c4696.woff"
+  },
+  {
+    "revision": "34cc487b3cb7d06dc0233644b2fd40da",
+    "url": "/crystalui/static/media/gothic.34cc487b.ttf"
+  },
+  {
+    "revision": "9a7bcf3c1eb7e01e00b68fb984bc2f2f",
+    "url": "/crystalui/static/media/gothic.9a7bcf3c.eot"
+  },
+  {
+    "revision": "f42346fdabdf59e4113ad71c2eabdb83",
+    "url": "/crystalui/static/media/me.f42346fd.png"
+  },
+  {
+    "revision": "fbf650933673be1b75d85376d5b79efd",
+    "url": "/crystalui/static/media/notFound.fbf65093.png"
+  },
+  {
+    "revision": "8e3f982bb515181178ce16902721136e",
+    "url": "/crystalui/static/media/space.8e3f982b.jpg"
+  }
+]);
