696f48c63f60889d13ffc9d39640c69be95ab487
unknown <soumen.rim@gmail.com>
2020-06-12 01:13:29 +0530

    Updates

---
+++ precache-manifest.b214629ae1e30814c4ee34f7673b9dc3.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9b743df1c74c95215fed7f5cb58228c8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a2d381f7a2735a04a4e7",
+    "url": "/static/css/main.4e876c23.chunk.css"
+  },
+  {
+    "revision": "52c4867dafbb8cbd4c2d",
+    "url": "/static/js/2.047c0aec.chunk.js"
+  },
+  {
+    "revision": "8a666dd660e108ab4f1e0f22fb2e9cd2",
+    "url": "/static/js/2.047c0aec.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a2d381f7a2735a04a4e7",
+    "url": "/static/js/main.ecf71476.chunk.js"
+  },
+  {
+    "revision": "9b390ab7ab89ae48edbf",
+    "url": "/static/js/runtime-main.43222647.js"
+  },
+  {
+    "revision": "fe9f4e04b009e13dceb57de2acf5b238",
+    "url": "/static/media/bg.fe9f4e04.jpg"
+  },
+  {
+    "revision": "151c4696847a325b6f5a672ec0c426bc",
+    "url": "/static/media/gothic.151c4696.woff"
+  },
+  {
+    "revision": "34cc487b3cb7d06dc0233644b2fd40da",
+    "url": "/static/media/gothic.34cc487b.ttf"
+  },
+  {
+    "revision": "9a7bcf3c1eb7e01e00b68fb984bc2f2f",
+    "url": "/static/media/gothic.9a7bcf3c.eot"
+  },
+  {
+    "revision": "f42346fdabdf59e4113ad71c2eabdb83",
+    "url": "/static/media/me.f42346fd.png"
+  },
+  {
+    "revision": "fbf650933673be1b75d85376d5b79efd",
+    "url": "/static/media/notFound.fbf65093.png"
+  },
+  {
+    "revision": "8e3f982bb515181178ce16902721136e",
+    "url": "/static/media/space.8e3f982b.jpg"
+  }
+]);
